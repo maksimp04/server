59d5e1206b1172e3aec77bd0ccfc3d7323d71cd9
maksimp04 <maksimp04@mail.ru>
2018-05-09 13:41:21 +0600

    well...

---
+++ ram500.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java.exe -Xmx630M -Xms500M -jar forge-1.12.2-14.23.3.2655-universal.jar
+PAUSE
---
+++ ram650.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java.exe -Xmx1024M -Xms650M -jar forge-1.12.2-14.23.3.2655-universal.jar
+PAUSE
